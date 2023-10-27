745c57380fc9e2e4b942f04fb2033fc26581615b
mohitkamboj2799 <133959924+mohitkamboj2799@users.noreply.github.com>
2023-10-27 12:20:46 +0530

    comiit

--- Script/start_container.sh
+++ Script/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull mohitkamboj2799/test_apache
+docker push mohitkamboj2799/simple-python-flask-app
 
 # Run the Docker image as a container
 docker run -it mohitkamboj2799/test_apache
