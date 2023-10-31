fd630904f096ddd37e7aaef69f080b4f4bff19c0
mohitkamboj2799 <133959924+mohitkamboj2799@users.noreply.github.com>
2023-10-31 15:06:40 +0530

    commit

---
+++ start_server.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+# Pull the Docker image from Docker Hub
+docker push mohitkamboj2799/simple-python-flask-app
+
+# Run the Docker image as a container
+docker run -it mohitkamboj2799/simple-python-flask-app
